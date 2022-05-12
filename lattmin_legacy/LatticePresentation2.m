c000365df7c6452a96bdad089942c5ad440dce26
DillonCislo <dilloncislo@gmail.com>
2022-05-12 16:53:10 -0700

    Improved lattice visualization code

--- lattmin_legacy/LatticePresentation2.m
+++ lattmin_legacy/LatticePresentation2.m
@@ -1,4 +1,4 @@
-function LatticePresentation2(g, varargin) 
+function LatticePresentation2(g, options) 
 % display Lattmin lattice 
 %
 % LatticePresentation2(g)
@@ -10,24 +10,30 @@ function LatticePresentation2(g, varargin)
 % -firstVertex:     color first vertex of the cell
 % -transparent:     transparent faces
 % -edgeColor:       color of edges
+% -edgeColorRange:  color range of lattice edges
 % -lineWidth:       edge thickness
 % -colorTable:      table of values for each cell to base coloring on
+% -cellColorRange:  color range of lattice faces
+% -includeEdgeColorbar: include a colorbar for edge based fields
+% -includeFaceColorbar: include a colorbar for face based fields
+% -edgeColormap:    colormap to use for edge based fields
+% -cellColormap:    colormap to use for face based fields
 
 maxNN = 14;
 cmp = lines(maxNN);
+cmp = [cmp; 0 0 0];
 
-if nargin == 2
-    options = varargin{1};
-else
-    options = struct('cellIndex', 0, 'firstVertex', 0);
-end
+if (nargin < 2), options = struct('cellIndex', 0, 'firstVertex', 0); end
 
 if isfield(options, 'edgeColor') && numel(options.edgeColor)==1
     
+    % Single number index into a saved colormap
+    % This option does not appear to be used...
     edgeColor = options.edgeColor;
     
 elseif isfield(options, 'edgeColor') && numel(options.edgeColor)>1
     
+    % Scalar data per edge with optional color re-scaling
     edgeColor = [0 0 0];
     CT = options.edgeColor;
     
@@ -35,16 +41,55 @@ elseif isfield(options, 'edgeColor') && numel(options.edgeColor)>1
         
         minVal = options.edgeColorRange(1);
         maxVal = options.edgeColorRange(2);
+
     else
         
         minVal = min(CT);
         maxVal = max(CT);
+
     end
-   
-	edgeCData = min(CT - minVal, maxVal-minVal)/(maxVal-minVal);
+
+    % Average edge data for bond-antibond pairs (only one color can be
+    % displayed per edge)
+    bonds = sort(g.bonds(:, 1:2), 2);
+    dupl = find_duplicate_rows(bonds);
+
+    duplIDx = struct2cell(dupl);
+    duplIDx = duplIDx(2,:).';
+    assert(all(cellfun(@numel, duplIDx) == 2), 'Non-manifold edge found');
+    duplIDx = [duplIDx{:}].';
+
+    antiBondIDx = repmat((1:size(g.bonds,1)).', 1, 2);
+    antiBondIDx(duplIDx, 1) = [duplIDx(:,2); duplIDx(:,1)];
+
+    CT = (CT + CT(antiBondIDx(:,2))) ./ 2;
+
+    rmIDx = antiBondIDx(:,1) > antiBondIDx(:,2);
+    bonds(rmIDx, :) = [];
+    CT(rmIDx, :) = [];
+
+	% edgeCData = min(CT - minVal, maxVal-minVal)/(maxVal-minVal);
+
+    CT(CT < minVal) = minVal;
+    CT(CT > maxVal) = maxVal;
+
+    if isfield(options, 'edgeColormap')
+        edge_cmap = options.edgeColormap;
+    else
+        edge_cmap = parula(256);
+    end
+
+    valsN = round(((CT - minVal) ./ (maxVal-minVal)) .* 255)+1;
+    valsN(isnan(valsN)) = 1;
+
+    edgeColors = edge_cmap(valsN,:);
+    edge_crange = [minVal maxVal];
     
 else
+
+    % Default to black edges
     edgeColor = [0 0 0];
+
 end
 
 if isfield(options, 'lineWidth')
@@ -57,63 +102,163 @@ end
 if isfield(options, 'colorTable')
 
     CT = options.colorTable(:);
-    minVal = -5;%-max(abs(CT))/2;%0;%min(CT);
-    maxVal = 5;%max(abs(CT))/2;%max(CT)+1;
+
+    if isfield(options, 'cellColorRange')
+
+        minVal = options.cellColorRange(1);
+        maxVal = options.cellColorRange(2);
+
+    else
+        
+        minVal = min(CT);
+        maxVal = max(CT);
+    end
+
     CT(CT < minVal) = minVal;
     CT(CT > maxVal) = maxVal;
-    cmap = jet(256);
-    colorsIdx = round(255*min(CT - minVal, maxVal-minVal)/(maxVal-minVal) + 1);
-    colors = cmap(colorsIdx,:);
+
+    if isfield(options, 'cellColormap')
+        cell_cmap = options.cellColormap;
+    else
+        cell_cmap = parula(256);
+    end
+
+    valsN = round(((CT - minVal) ./ (maxVal-minVal)) .* ...
+        (size(cell_cmap,1)-1))+1;
+    valsN(isnan(valsN)) = 1;
+
+    cellColors = cell_cmap(valsN,:);
+    cell_crange = [minVal maxVal];
     
 % else contruct one from neighbor number
 else
-    colors = zeros([length(g.cells) 3]);
-    
-    for ci = 1 : length(g.cells)
-        
-        if ~isempty(g.cells{ci})
-            
-            verts = (g.bonds(g.cells{ci}(:),1:2));
-            verts = verts(:,1);
-        
-            % use black for cells with too many neighbors
-            if length(verts) <= maxNN
-                colors(ci,:) = cmp(length(verts),:);
-            else
-                colors(ci,:) = [0 0 0];
-            end
-        end
-    end
+
+    % NOTE: This assumes no boundary antibonds are maintained
+    bdyBondIDx = find((g.bonds(:,3) == 0) | (g.bonds(:,4) == 0));
+
+    cellColors = cellfun(@(x) numel(x) - sum(ismember(x, bdyBondIDx)), ...
+        g.cells).';
+    cellColors(cellColors > maxNN) = maxNN + 1;
+    cellColors = cmp(cellColors, :);
+    cell_cmap = cmp;
+    cell_crange = [1 maxNN];
+
+    % cellColors = zeros([length(g.cells) 3]);
+    % 
+    % for ci = 1:length(g.cells)
+    %     
+    %     if ~isempty(g.cells{ci})
+    %         
+    %         verts = (g.bonds(g.cells{ci}(:),1:2));
+    %         verts = verts(:,1);
+    %     
+    %         % use black for cells with too many neighbors
+    %         if length(verts) <= maxNN
+    %             cellColors(ci,:) = cmp(length(verts),:);
+    %         else
+    %             cellColors(ci,:) = [0 0 0];
+    %         end
+    %     end
+    %   
+    % end
+
 end
 
-% visualize cells
+if ~isfield(options, 'transparent')
+    options.transparent = false;
+end
+
+% Transparent face choice overrides face color bar option
+if (~isfield(options, 'includeFaceColorbar') || ...
+        options.transparent)
+    options.includeFaceColorbar = false;
+end
+
+% Face color bar option overrides edge color bar option
+if (~isfield(options, 'includeEdgeColorbar') || ...
+        options.includeFaceColorbar)
+    options.includeEdgeColorbar = false;
+end
+
+
+% Convert the cell connectivity list to a (NaN-padded)
+% matrix Useful for plotting purposes
+maxFaceSize = max(cellfun(@numel, g.cells));
+cellFace = nan(numel(g.cells), maxFaceSize);
+% bondv = g.bonds(:, 1:2);
+for i = 1:size(cellFace,1)
+    cellFace(i, 1:numel(g.cells{i})) = g.bonds(g.cells{i}, 1).';
+end
+
+% Visualize cells
+dispCells = ~cellfun(@isempty, g.cells).';
+if options.transparent
+
+    patch('Faces', cellFace(dispCells, :), 'Vertices', g.verts(:, 1:2), ...
+        'FaceColor', 'none', 'EdgeColor', edgeColor, 'LineWidth', lw);
+
+else
+
+    patch('Faces', cellFace(dispCells, :), 'Vertices', g.verts(:, 1:2), ...
+        'FaceVertexCData', cellColors, 'FaceColor', 'flat', ...
+        'EdgeColor', edgeColor, 'LineWidth', lw);
+
+end
+
+if (isfield(options, 'edgeColor') && numel(options.edgeColor) > 1)
+
+    hold on
+    patch( 'Faces', reshape([1:size(bonds,1),1:size(bonds,1)*2],[],3), ...
+        'Vertices', [g.verts(bonds(:), 1), g.verts(bonds(:), 2)], ...
+        'FaceVertexCData', repmat(edgeColors, 2, 1), ...
+        'EdgeColor', 'interp' );
+    hold off
+
+end
+
+if options.includeFaceColorbar
+    cb = colorbar;
+    colormap(cell_cmap);
+    set(gca, 'Clim', cell_crange);
+end
+
+if options.includeEdgeColorbar
+    cb = colorbar;
+    colormap(edge_cmap);
+    set(gca, 'Clim', edge_crange);
+end
+
+
+% OLD SLOW VERSION WITH MULTIPLE PATCH OBJECTS ****************************
 % TODO: this would probably be faster using the fv structure
-for ci = 1 : length(g.cells)
+% for ci = 1 : length(g.cells)
+% 
+%     if ~isempty(g.cells{ci})
+%         
+%         verts = g.bonds(g.cells{ci}(:), 1:2);
+%         verts = verts(:,1);
+% 
+%         if (isfield(options, 'transparent') && options.transparent) ||...
+%            (isfield(options, 'transparentColor') && options.transparentColor == CT(ci))
+%        
+%             patch(g.verts(verts,1), g.verts(verts,2), cellColors(ci,:),...
+%                     'EdgeColor',edgeColor,'FaceColor','None','LineWidth',lw);
+%         else
+%             patch(g.verts(verts,1),g.verts(verts,2), cellColors(ci,:),...
+%                 'EdgeColor',edgeColor,'FaceColor', cellColors(ci,:),'LineWidth',lw);
+%         end
+%         
+%         if isfield(options,'edgeColor') && numel(options.edgeColor) > 1
+%             cdata = edgeCData(circshift(g.cells{ci},[0 0]),:);
+%             patch(g.verts(verts,1), g.verts(verts,2), cdata,...
+%                     'EdgeColor','flat','FaceColor','None','LineWidth',2,...
+%                     'CDataMapping','scaled');
+%         end
+%     end
+% end
+% caxis([0 1]);
+% *************************************************************************
 
-    if ~isempty(g.cells{ci})
-        
-        verts = (g.bonds(g.cells{ci}(:),1:2));
-        verts = verts(:,1);
-
-        if (isfield(options, 'transparent') && options.transparent) ||...
-           (isfield(options, 'transparentColor') && options.transparentColor == CT(ci))
-       
-            patch(g.verts(verts,1), g.verts(verts,2), colors(ci,:),...
-                    'EdgeColor',edgeColor,'FaceColor','None','LineWidth',lw);
-        else
-            patch(g.verts(verts,1),g.verts(verts,2), colors(ci,:),...
-                'EdgeColor',edgeColor,'FaceColor', colors(ci,:),'LineWidth',lw);
-        end
-        
-        if isfield(options,'edgeColor') && numel(options.edgeColor) > 1
-            cdata = edgeCData(circshift(g.cells{ci},[0 0]),:);
-            patch(g.verts(verts,1), g.verts(verts,2), cdata,...
-                    'EdgeColor','flat','FaceColor','None','LineWidth',2,...
-                    'CDataMapping','scaled');
-        end
-    end
-end
-caxis([0 1]);
 axis equal
 
 % visualize boundary bonds
@@ -127,10 +272,15 @@ for ci = 1:length(g.bdryBonds)
         g.verts(v2,1)- g.verts(v1,1), g.verts(v2,2)-g.verts(v1,2), 0,...
                                 'Color', edgeColor, 'LineWidth', lw);
     hold off;
+    
+end
+
+if ~isfield(options, 'cellIndex')
+    options.cellIndex = false;
 end
 
 % label cells by their index 
-if isfield(options, 'cellIndex')
+if options.cellIndex
     
     nCells = numel(g.cells);
     if numel(options.cellIndex) == 1 && options.cellIndex
@@ -167,3 +317,34 @@ end
 set(gcf,'Color','white');
 
 end
+
+function [ dupl, C ] = find_duplicate_rows( A )
+%FIND_DUPLICATE_ROWS Finds duplicate rows in a matrix
+
+[C, ia, ~] = unique( A, 'rows' );
+
+if size(A,1) == size(C,1)
+    % disp('There are no duplicate rows!');
+    dupl = {};
+    return;
+end
+
+rep_idx = setdiff(1:size(A,1), ia);
+rep_val = unique( A(rep_idx,:), 'rows');
+
+dupl_val = cell( size(rep_val,1), 1 );
+dupl_idx = cell( size(rep_val,1), 1 );
+
+for i = 1:size(rep_val,1)
+    
+    dupl_val{i} = rep_val(i,:);
+    
+    diffA = A - repmat( rep_val(i,:), size(A,1), 1 );
+    dupl_idx{i} = find( sqrt(sum(diffA .* conj(diffA), 2)) < eps );
+    
+end
+
+dupl = struct( 'val', dupl_val, 'idx', dupl_idx );
+
+end
+
